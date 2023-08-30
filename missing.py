c85d81f20c21bcc391cd4c2539a31803179603ba
TUSHAR KHATRI <77196184+tushar718@users.noreply.github.com>
2023-08-30 13:45:56 +0530

    Add files via upload

---
+++ missing.py
@@ -0,0 +1,7 @@
+def writemissing(title,bm):
+    if(bm=='movie'):
+        with open('missingmovie.csv','a') as f:
+            f.write(title)
+    else:
+        with open('missingbook.csv','a') as f:
+            f.write(title)
